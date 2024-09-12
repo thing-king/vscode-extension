c9865bdee35b8af0bf91187bd4f23d64d690eb76
savannt <savant.eclipse@gmail.com>
2024-09-12 17:24:19 -0400

    Test

--- extension.js
+++ extension.js
@@ -1,28 +1,32 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Your e xtension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-extension" is now active!');
+	console.log(
+		'Congratulations, your extension "vscode-extension" is now active!'
+	);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	const disposable = vscode.commands.registerCommand(
+		"vscode-extension.helloWorld",
+		function () {
+			// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Thingking!');
-	});
+			// Display a message box to the user
+			vscode.window.showInformationMessage("Hello World from Thingking!");
+		}
+	);
 
 	context.subscriptions.push(disposable);
 }
@@ -32,5 +36,5 @@ function deactivate() {}
 
 module.exports = {
 	activate,
-	deactivate
-}
+	deactivate,
+};
