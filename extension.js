a9992745df77254cc5b8f0e104ed329641e3d77f
savannt <savant.eclipse@gmail.com>
2024-09-12 17:25:56 -0400

    test

--- extension.js
+++ extension.js
@@ -21,7 +21,7 @@ function activate(context) {
 	const disposable = vscode.commands.registerCommand(
 		"vscode-extension.helloWorld",
 		function () {
-			// The code you place here will be executed every time your command is executed
+			// The code you place here will  be executed every time your command is executed
 
 			// Display a message box to the user
 			vscode.window.showInformationMessage("Hello World from Thingking!");
